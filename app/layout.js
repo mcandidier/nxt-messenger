8a334148a907a19e93cecd09ebeae207e8c96f64
dev <mannycandidier@gmail.com>
2023-10-10 21:05:28 +0800

    meta requests

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,9 @@ export default async function RootLayout({
   
   return (
     <html lang="en">
+      <head>
+       <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> 
+      </head>
       <body>
       <Providers>
           <Toaster/>
